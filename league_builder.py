c0e5cf6ffadde6dcb3de33288b2454af628aa379
Kirsten Schwarzer <kirstenschwarzer@gmail.com>
2018-07-22 16:42:36 +0200

    Create teams.txt file

--- league_builder.py
+++ league_builder.py
@@ -10,12 +10,11 @@ my_teams = {}
 
 # Open file
 def open_mycsv():
-    with open('soccer_players.csv', 'r') as csvfile:
+    with open('/Users/kirstenschwarzer/Code/treehouse-project-1/soccer_players.csv', 'r') as csvfile:
         playerreader = csv.reader(csvfile)
         for row in playerreader:
             player_list.append(row)
         csvfile.close()
-
 
 # Sort players by experience
 def experience(player_list, experienced_players, inexperienced_players):
@@ -26,7 +25,7 @@ def experience(player_list, experienced_players, inexperienced_players):
             if 'NO' in attribute:
                 inexperienced_players.append(row)
 
-# Divide players into teams
+# Divide players into teams and write to teams.txt file
 def divide_teams(experienced_players, inexperienced_players):
     raptors = experienced_players[::3] + inexperienced_players[::3]
     sharks = experienced_players[1::3] + inexperienced_players[1::3]
@@ -34,8 +33,8 @@ def divide_teams(experienced_players, inexperienced_players):
 
     my_teams = {"Raptors": raptors, "Sharks": sharks, "Dragons": dragons}
 
-# Create output teams.txt file
-    with open("teams.txt", "a") as teamfile:
+    # Create output teams.txt file
+    with open("/Users/kirstenschwarzer/Code/treehouse-project-1/teams.txt", "a") as teamfile:
         for name, players in my_teams.items():
             teamfile.write(name + "\n")
             for player in players:
@@ -43,7 +42,6 @@ def divide_teams(experienced_players, inexperienced_players):
             teamfile.write("\n")
 
     teamfile.close()
-
 
 if __name__ == '__main__':
     open_mycsv()
